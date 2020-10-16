dc682ed40b62510346d9b3033afeeccbf55c9e14
autumn0leaves9 <72963877+autumn0leaves9@users.noreply.github.com>
2020-10-16 16:04:19 +0900

    Create SoundCreatephp.php

---
+++ SoundCreatephp.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>BANBOO EFFECTS</title>
+    <link rel="stylesheet" href="SoundCreateCSS.css" >
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  </head>
+  <body link="#fff" vlink="#fff" alink="blue">
+    <header>
+      <div class="logo"><a href="SoundCreate.html" style="text-decoration:none;">BANBOO EFFECTS</a></div>
+      <ul class="headerli">
+        <li><a href="effect.html">Effects</a></li>
+		<li>creator</li>
+        <li>Teams of servis</li>
+        <li>Contact</li>
+		<li><a href="https://twitter.com/Under_Bamboo_TL?s=20">Twitter</a></li>
+		
+      </ul>
+    </header>
+	
+	<div class="main">
+	
+      <div class="container">
+		 <h2><span>BANBOO EFFECTS</span>へようこそ</h2>
+      </div>
+	  
+	  <div class="portfolio">
+	  
+	  
+	  <ul class="flex-list">
+	  
+		<li class="flex-li1">portfolio</li>
+		
+		<li class="flex-li2">最新動画</li>
+		
+		<li class="flex-li3"><video src=".\video\portfolio.mp4" 
+			alt="flex-li3" width="480px" height="auto" controls="controls"></video></li>
+			
+		<li class="flex-li4">
+			<iframe alt="flex-li4" width="480" height="270" src="https://www.youtube.com/embed/o7QyCEx81rM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></li>
+	  </ul>
+	  </div>
+	  
+	  
+	  <div class="contents">
+        <h3 class="contents-title">カテゴリ</h3>
+      </div>
+	  
+	  <div class="contents-item">
+        <img src=".\img\item1.jpg" alt="item1"  width="220px" height="220px">
+        <p><a href="effect.html">効果音</a></p>
+      </div>
+      
+      <div class="contents-item">
+        <img src=".\img\item2.jpg" alt="item2" width="220px" height="220px">
+        <p>素材</p>
+      </div>
+      
+      <div class="contents-item">
+        <img src=".\img\item3.jpg" alt="item3" width="220px" height="220px">
+        <p>BGM</p>
+      </div>
+      
+      <div class="contents-item">
+        <img src=".\img\item4.jpg" alt="item4" width="220px" height="220px">
+        <p>テロップ</p>
+      </div>
+	  
+	  <div class="contact-form">
+        <h3 class="contact-title">お問い合わせ</h3>
+		
+		<form action="sent.php" method="post">
+		
+		<p>名前</p>
+		<input name="name" type="text">
+		
+        <p>メールアドレス（必須）</p>
+       <input name="mail" type="text">
+        
+        <p>お問い合わせ内容（必須）</p>
+        <textarea name="body"></textarea>
+        
+        <p>※必須項目は必ずご入力ください</p>
+        <input class="contact-submit" type="submit" value="送信">
+        
+      </div>
+	  
+    </div>
+	
+	<footer>
+     <div class="footer-logo">松竹</div>
+      <ul class="footerli">
+        <li>会社概要</li>
+        <li>採用</li>
+        <li>お問い合わせ</li>
+      </ul>
+    </footer>
+	
+  </body>
+</html>
